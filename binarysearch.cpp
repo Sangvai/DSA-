093205849beb7601b286e05cdf2c7146dcbd4959
Sanika Sangvai <sanikasangvai@gmail.com>
2026-09-17 01:37:28 +0530

    Add new DSA solution

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int binarysearch(int arr[], int n, int key){
     int s=0;
     int e = n-1;
-     int mid= (s+e)/2;
+     int mid= s+(e-s)/2;
      while(s<=e){
         if(arr[mid]==key){
             return mid;
@@ -14,7 +14,7 @@ int binarysearch(int arr[], int n, int key){
         else{
             s = mid+1;
         }
-        mid= (s+e)/2;
+        mid= s+(e-s)/2;
      }
      return -1;
 }
---
+++ math.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, temp, c=0; 
+    cin>>n;
+    cout<<(int)(log(n)+1);
+    return 0;
+}
